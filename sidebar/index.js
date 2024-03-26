68ad8e218d5df2a7b57c3e8954c6faed08cd9c06
Yerkebulan <erkebulanlbk@gmail.com>
2024-03-26 16:31:50 +0500

    cjanges

--- sidebar/index.js
+++
@@ -1,8 +0,0 @@
-function openSidebar() {
-  document.getElementById("sidebar").style.width = "250px";
-  document.getElementsByClassName("content")[0].style.marginLeft = "250px";
-}
-function closeSidebar() {
-  document.getElementById("sidebar").style.width = "0";
-  document.getElementsByClassName("content")[0].style.marginLeft = "0";
-}
